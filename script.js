1feccc94a70025735bdb94d49bd2b685192a0148
meethatkar <meethatkar000@gmail.com>
2025-08-31 21:29:26 +0530

    added other animation

--- script.js
+++ script.js
@@ -37,20 +37,19 @@ function hero_amin() {
     })
 }
 
-function image_effect(){
-    Shery.imageEffect("#textNimg img", {
-        style: 1,
-        // gooey:true,
-        debug: true,
-      });
-}
+
+Shery.imageEffect("#textNimg img", {
+    style: 3,
+    config: { "uFrequencyX": { "value": 12, "range": [0, 100] }, "uFrequencyY": { "value": 13.74, "range": [0, 100] }, "uFrequencyZ": { "value": 16.03, "range": [0, 100] }, "geoVertex": { "range": [1, 64], "value": 32 }, "zindex": { "value": "1", "range": [-9999999, 9999999] }, "aspect": { "value": 0.750010874328253 }, "ignoreShapeAspect": { "value": true }, "shapePosition": { "value": { "x": 0, "y": 0.05 } }, "shapeScale": { "value": { "x": 0.5, "y": 0.5 } }, "shapeEdgeSoftness": { "value": 0, "range": [0, 0.5] }, "shapeRadius": { "value": 0, "range": [0, 2] }, "currentScroll": { "value": 0 }, "scrollLerp": { "value": 0.07 }, "gooey": { "value": false }, "infiniteGooey": { "value": false }, "growSize": { "value": 4, "range": [1, 15] }, "durationOut": { "value": 1, "range": [0.1, 5] }, "durationIn": { "value": 1.5, "range": [0.1, 5] }, "displaceAmount": { "value": 0.5 }, "masker": { "value": true }, "maskVal": { "value": 1.5, "range": [1, 5] }, "scrollType": { "value": 0 }, "noEffectGooey": { "value": true }, "onMouse": { "value": 1 }, "noise_speed": { "value": 0.2, "range": [0, 10] }, "metaball": { "value": 0.2, "range": [0, 2] }, "discard_threshold": { "value": 0.5, "range": [0, 1] }, "antialias_threshold": { "value": 0.002, "range": [0, 0.1] }, "noise_height": { "value": 0.5, "range": [0, 2] }, "noise_scale": { "value": 10, "range": [0, 100] } }
+    // debug:true
+})
+
+Shery.imageEffect(".img-anim img", {
+    style: 5,
+    // config:{}
+})
 
 document.addEventListener("DOMContentLoaded", () => {
     nav_anim();
     hero_amin();
-})
-
-window.addEventListener("load",()=>{
-    text_animate();
-    image_effect();
 })
